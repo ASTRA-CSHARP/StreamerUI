f187c2d078109789be9f7853e25868884b7e4de3
ASTRA-CSHARP <titaniumgtags@hotmail.com>
2024-09-04 21:59:28 +0100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,95 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+using Photon.Pun;
+using UnityEngine.InputSystem;
+using GorillaNetworking;
+using static Photon.Pun.UtilityScripts.TabViewManager;
+using Steamworks;
+using Unity.Burst.Intrinsics;
+namespace StreamerUI
+{
+
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool open;
+        bool hidecode;
+        static string display;
+        int lolz;
+        Rect window = new Rect(10f, 10f, 200f, 200f);
+
+        void Start()
+        {
+            
+        }
+
+        void OnEnable()
+        {
+            
+
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        void OnDisable()
+        {
+            
+
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            
+        }
+
+        void Update()
+        {
+            if (Keyboard.current.tabKey.wasPressedThisFrame)
+            {
+                open = !open;
+            }
+            
+            
+        }
+
+        void OnGUI()
+        {
+            GUI.backgroundColor = Color.white;
+            GUI.skin.textArea.fontStyle = FontStyle.Bold;
+            if (!open)
+            {
+                if (PhotonNetwork.InRoom)
+                {
+                    window = GUI.Window(lolz, window, new GUI.WindowFunction(drawTabs), "<color=black>STREAMER UI</color>");
+                }
+            }
+        }
+
+        void drawTabs(int lolz)
+        {
+            display = GUI.TextArea(new Rect(10f, 40f, 160f, 50f), display);
+
+            display = string.Concat(new string[]
+            {
+                    "CURRENT ROOM: ",
+                    PhotonNetwork.CurrentRoom.Name,
+                    "\nPLAYER COUNT: ",
+                    PhotonNetwork.CurrentRoom.PlayerCount.ToString(),
+                    "/10\nQUEUE: ",
+                    GorillaComputer.instance.currentQueue
+
+            });
+
+            hidecode = GUI.Toggle(new Rect(10f, 165f, 160f, 15f), hidecode, "HIDE ROOM CODE");
+            if (hidecode)
+            {
+                display = "HIDDEN ROOM DATA";
+            }
+            GUI.DragWindow();
+        }
+
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace StreamerUI
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.astraa.gorillatag.streamerui";
+        public const string Name = "StreamerUI";
+        public const string Version = "1.0.0";
+    }
+}
